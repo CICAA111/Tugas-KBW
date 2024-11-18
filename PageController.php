c0dfb6fcf203071216f03747d17c7842f2128a11
Chicakowi <138846271+CICAA111@users.noreply.github.com>
2024-11-18 14:28:31 +0700

    Update PageController.php

--- PageController.php
+++ PageController.php
@@ -1,12 +1,23 @@
-<?php
+<?php  
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers;  
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;  
 
-class PageController extends Controller
-{
-    public function home{}{
-        return view {'home'};
-    }
+class PageController extends Controller  
+{  
+    public function home()  
+    {  
+        return view('home');  
+    }  
+
+    public function about()  
+    {  
+        return view('about');  
+    }  
+
+    public function contact()  
+    {  
+        return view('contact');  
+    }  
 }
